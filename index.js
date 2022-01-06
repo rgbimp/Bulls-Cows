164a5f0df62662e7343a7aba758b1f8be5cb4053
AlenaKuznetsova <rgb.imp@gmail.com>
2022-01-06 17:56:05 +0300

    add check input numbers

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const COUNT_SHOTS = 10;
 const readlineSync = require('readline-sync');
 
-const countDigits = readlineSync.question('Выберите сложность (число от 3 до 6): ')
+let countDigits = readlineSync.question('---------------\nSelect number from 3 to 6: \n')
+while (countDigits > 6 || countDigits < 3) {
+  console.log('Incorrect number!!!\n---------------\n')
+  countDigits = readlineSync.question('Select number from 3 to 6: \n')
+}
 
 const createHiddenNumber = (countDigits) => {
   let arr = [];
@@ -17,8 +21,11 @@ const createHiddenNumber = (countDigits) => {
 
 const hiddenNumber = createHiddenNumber(countDigits);
 
-console.log(`Загадано число из ${countDigits} цифр. 
-              Попробуйте его угадать!`);
+console.log(`---------------\nTry to guess the number of ${countDigits} digits. \nThe digits aren't repeated!`);
 
-const answerNumber = readlineSync.question('Ваш ответ: ');
+let answerNumber = readlineSync.question('Your answer: ').split('').map(item => +item);
+while (answerNumber.length != hiddenNumber.length) {
+  console.log('Incorrect amount of digits in the number!!!')
+  answerNumber = readlineSync.question('Your answer: ').split('').map(item => +item);
+}
 
