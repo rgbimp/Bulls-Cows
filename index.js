ecfe04bd319c2c8bc9bc465a2fc46bb337d7f5a3
AlenaKuznetsova <rgb.imp@gmail.com>
2022-01-06 19:02:10 +0300

    add logic of the play

--- index.js
+++ index.js
@@ -21,11 +21,39 @@ const createHiddenNumber = (countDigits) => {
 
 const hiddenNumber = createHiddenNumber(countDigits);
 
-console.log(`---------------\nTry to guess the number of ${countDigits} digits. \nThe digits aren't repeated!`);
+console.log(`---------------
+Try to guess the number of ${countDigits} digits. 
+The digits aren't repeated!
+You have ${COUNT_SHOTS} shots.`);
 
-let answerNumber = readlineSync.question('Your answer: ').split('').map(item => +item);
-while (answerNumber.length != hiddenNumber.length) {
-  console.log('Incorrect amount of digits in the number!!!')
-  answerNumber = readlineSync.question('Your answer: ').split('').map(item => +item);
+let isWin = true;
+
+for (let i = 0; i < COUNT_SHOTS; i++) {
+  let answerNumber = readlineSync.question('Your answer: ').split('').map(item => +item);
+  while (answerNumber.length != hiddenNumber.length) {
+    console.log('Incorrect amount of digits in the number!!!')
+    answerNumber = readlineSync.question('Your answer: ').split('').map(item => +item);
+  }
+  let bulls = 0;
+  let cows = 0;
+
+  if (hiddenNumber.join('') === answerNumber.join('')) {
+      isWin = true;
+      break;
+  } else {
+    answerNumber.forEach((item, index) => {
+      if (hiddenNumber.includes(item)) {
+        (index === hiddenNumber.indexOf(item)) ? bulls++ :  cows++;
+      }
+    })
+    isWin = false;
+    console.log(`Amount of matching digits in their places: ${bulls}
+Amount of matching digits in the wrong places: ${cows}
+Try again! You have ${COUNT_SHOTS - 1 - i} shots left`)
+  }
 }
 
+(isWin) 
+  ? console.log(`Number was guessed!\nYOU WIN!`)
+  : console.log(`The shots are over.\n You lose:(`)
+console.log('---------------')
